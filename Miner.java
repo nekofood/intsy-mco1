806ea5666503bb9caf90558677a55d50c00010bd
dale <sonicroblox@gmail.com>
2021-11-24 15:09:01 +0800

    added x,y

--- Miner.java
+++ Miner.java
@@ -1,8 +1,11 @@
 public class Miner {
 	private int rotation; //0deg = right, 90deg = up, and so on
+	private int x,y;
 
-	Miner() {
+	Miner(int sx, int sy) {
 		rotation = 0;
+		x = sx;
+		y = sy;
 	}
 
 	//Rotate counter-clockwise
