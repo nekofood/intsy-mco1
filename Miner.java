6b9b73be4453552e3000f944d4a5a7ee51beda08
dale <sonicroblox@gmail.com>
2021-11-24 14:36:42 +0800

    stuff

--- Miner.java
+++ Miner.java
@@ -1,3 +1,12 @@
 public class Miner {
+	private int rotation; //0deg = right, 90deg = up, and so on
 
+	Miner() {
+		rotation = 0;
+	}
+
+	//Rotate counter-clockwise
+	public void rotate() {
+		rotation = (rotation+90)%360;
+	}
 }
