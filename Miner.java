c0bfd862c0f3b56dcd459fb48d6ce3512493fa14
dale <sonicroblox@gmail.com>
2021-11-24 15:22:35 +0800

    forward()

--- Miner.java
+++ Miner.java
@@ -12,4 +12,23 @@ public class Miner {
 	public void rotate() {
 		rotation = (rotation+90)%360;
 	}
+
+	//Move forward
+	//Note: does not handle collision yet
+	public void forward() {
+		switch (rotation) {
+			case 0:
+				++x; break;
+			case 90:
+				--y; break;
+			case 180:
+				--x; break;
+			case 270:
+				++y; break;
+			default:
+				System.out.println("Miner is facing invalid direction");
+				rotation = 0;
+				break;
+		}
+	}
 }
