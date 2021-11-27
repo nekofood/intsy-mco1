efb7b0dbcf073d66aee1377ff186f0dd291489ab
dale <sonicroblox@gmail.com>
2021-11-27 23:54:49 +0800

    generate gold

--- Grid.java
+++ Grid.java
@@ -1,5 +1,7 @@
+import java.lang.Math.*;
+
 public class Grid {
-	private int width, height;
+	private int n;
 	/* The things on the grid, such as beacons, pits, and the pot of gold, will
 	   be represented with a 2D array of chars, using chars to indicate the positions
 	   of things.
@@ -9,15 +11,18 @@ public class Grid {
 	   G = gold */
 	private char[][] grid;
 
-	Grid(int x, int y) {
-		width = x;
-		height = y;
-		grid = new char[y][x];
+	Grid(int length) {
+		n = length;
+		grid = new char[n][n];
 		grid[0][0] = 'M';
 	}
 
 	//TODO: "distribute" method to place the things on the grid
 	public void generateThings() {
-
+		//generate random numbers from 1 to n to place pot
+		int range = (n - 1) + 1;
+		int x = (int)(Math.random() * range) + 1;
+		int y = (int)(Math.random() * range) + 1;
+		grid[y][x] = 'G';
 	}
 }
