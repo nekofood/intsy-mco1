1b8e950000ee54c40b7b0b5afe767ed59fe5b32e
dale <sonicroblox@gmail.com>
2021-11-29 19:04:22 +0800

    misc

--- Controller.java
+++ Controller.java
@@ -2,13 +2,23 @@ import java.awt.event.*;
 import javax.swing.*;
 
 //the C in MVC
-//this class will be responsible for running the simulation and maybe running the agent
+//this class will be responsible for running the simulation/the agent
 class Controller {
 	private Grid grid;
 	private View view;
+	private boolean fastPlay; //is the simulation in "fast play" mode?
 
 	Controller(Grid g, View v) {
 		grid = g;
 		view = v;
+	}
+
+	//IDEA: while-true loop to run the simulation in fast mode?
+
+	//listener for the control window
+	class ControlListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			//blah blah blah
+		}
 	}
 }
