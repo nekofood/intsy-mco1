10277a4b1059c30f76008bccc2c11448c92c239d
dale <sonicroblox@gmail.com>
2021-11-29 10:00:39 +0800

    create ControlView.java

---
+++ ControlView.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//this class contains the window with the step, pause, speed controls, etc
+public class ControlView extends JFrame{
+
+}
