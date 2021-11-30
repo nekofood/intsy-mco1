c7ad89d9d95f78c3d9fa3fd12a9f2268f8c88f79
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2021-11-30 17:56:05 +0800

    Update Searcher.java

--- Searcher.java
+++ Searcher.java
@@ -3,16 +3,23 @@ import java.util.*;
 public class Searcher {
     private Grid grid;
     private Miner miner;
-    private ArrayList<Integer> moveSet; //where the moves will be stored
+    private ArrayList<Integer> moveSet; /*  where the moves will be stored
+                                            direction of move will be stored*/
+    private int nMoveCount;
 
     Searcher (Grid g, Miner m) {
         grid = g;
         miner = m;
         moveSet = new ArrayList<Integer> ();
+        nMoveCount = 0;
     }
 
     //The main searching alg will be here
     public void Search () {
 
+    }
+
+    private void addMove (int nDirection) {
+        moveSet.add (nDirection);
     }
 }
