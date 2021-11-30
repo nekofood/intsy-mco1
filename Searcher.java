51da12603bc7d169c361ca7b95eb8749f84e0f8c
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2021-11-30 21:21:50 +0800

    added sum stuff in random search

--- Searcher.java
+++ Searcher.java
@@ -3,20 +3,30 @@ import java.util.*;
 public class Searcher {
     private Grid grid;
     private Miner miner;
-    private ArrayList<Integer> moveSet; /*  where the moves will be stored
+    private ArrayList<Integer> smartMoveSet; /*  where the moves will be stored
                                             direction of move will be stored*/
-    private int nMoveCount;
+    private ArrayList<Integer> randomMoveSet;
+    private int nSmartCount;
+    private int nRandCount;
 
     Searcher (Grid g, Miner m) {
         grid = g;
         miner = m;
-        moveSet = new ArrayList<Integer> ();
-        nMoveCount = 0;
+        smartMoveSet = new ArrayList<Integer> ();
+        randomMoveSet = new ArrayList<Integer> ();
+        nSmartCount = 0;
+        nRandCount = 0;
     }
 
     //The main searching alg will be here
     public void Search () {
+        int nDirection = miner.getRotation ();
 
+
+
+        miner.forward ();
+        addMove (smartMoveSet, nDirection);
+        nSmartCount++;
     }
 
 	//The "random action" alg goes here
@@ -31,10 +41,12 @@ public class Searcher {
             miner.rotate ();
         
         miner.forward ();
+        addMove (randomMoveSet, nDirection);
+        nRandCount++;
 	}
 
     //This adds the move done to the list
-    private void addMove (int nDirection) {
+    private void addMove (ArrayList<Integer> moveSet, int nDirection) {
         moveSet.add (nDirection);
     }
 }
