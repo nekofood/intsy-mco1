6ab60dd82bf5502afdc640f393f8b7c9b07f6776
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2021-12-01 00:44:21 +0800

    this do be wat im usin to test things atm

--- GridDriver.java
+++ GridDriver.java
@@ -1,12 +1,25 @@
-//This just where i tested the grid thingies
+//This just where i tested the anything rly
+
+import java.util.*;
 
 public class GridDriver {
     final int MAX = 8;
 
     public static void main (String[] args) {
+        Scanner kb = new Scanner (System.in);
         Miner mi = new Miner(0, 0);
         Grid board = new Grid (8);
+        Searcher se = new Searcher (board, mi);
+        int i = 0;
 
-        board.printGrid ();
+        while (i++ != 10) {
+            se.Search ();
+            se.getGrid ().printGrid ();
+
+            kb.nextLine ();     //just keep pressing enter
+        }
+
+        se.printList ();    //Shows the directions it moved
+
     }
 }
