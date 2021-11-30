cfee8edc4228222f6408a44cdab29457883449e2
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2021-11-30 17:47:22 +0800

    Create Searcher.java

---
+++ Searcher.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Searcher {
+    private Grid grid;
+    private Miner miner;
+    private ArrayList<Integer> moveSet; //where the moves will be stored
+
+    Searcher (Grid g, Miner m) {
+        grid = g;
+        miner = m;
+        moveSet = new ArrayList<Integer> ();
+    }
+
+    //The main searching alg will be here
+    public void Search () {
+
+    }
+}
