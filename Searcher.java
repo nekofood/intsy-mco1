3fb72e4987dbb12d8aba53a56a968d3a4cc1e665
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2021-12-01 11:38:33 +0800

    prototype smartSearch() is here

--- Searcher.java
+++ Searcher.java
@@ -11,6 +11,8 @@ public class Searcher {
     private int n;
     private char cCurBlock;     //Current block Miner is on
     private boolean bSmart;      //Current agent. || T = Smart, F = Mentally inferior
+    private boolean bFoundGoldDir;
+    private boolean bFoundBeaconDir;
 
     Searcher (Grid g, Miner m) {
         grid = g;
@@ -22,6 +24,8 @@ public class Searcher {
         nRandCount = 0;
         cCurBlock = '\0';
         bSmart = false;
+        bFoundGoldDir = false;
+        bFoundBeaconDir = false;
     }
 
     //A func to check if it won or lost alrdy
@@ -65,18 +69,23 @@ public class Searcher {
     //The main searching alg will be here
     private void smartSearch () {
         int i;  //just for any loop used here
-        int nDirection = miner.getRotation ();
+        int nNewDirection;
         /*  This where it keeps tabs on scan result in what direction. [0] right, [1] top ...
             'L' just means Miner is on the edge of that direction already, otherwise it puts the letter */
         char[] scannedDirection = new char[4];
 
-        scanAllDirection (scannedDirection);
+        //if it found gold, it would stop scanning and just go on straight line
+        //this is an efficiency thing so dont mind the 'if' condition for now
+        //if (!bFoundGoldDir) {
+            scanAllDirection (scannedDirection);
+            nNewDirection = pickDirection (scannedDirection);
+            nNewDirection *= 90;
 
+            while (miner.getRotation() != nNewDirection)
+                miner.rotate ();
+        //}
 
-
-        miner.forward ();
-        grid.updateMinerPosition (miner.getX (), miner.getY ());
-        addMove (smartMoveSet, nDirection);
+        moveMiner ();
         nSmartCount++;
     }
 
@@ -84,7 +93,7 @@ public class Searcher {
 	private void randomSearch () {
         int[] directions = {0, 90, 180, 270};
         int nPick, nDirection;
-        boolean bValidMove = false;     
+        boolean bValidMove = false;
 
         do {
             nPick = (int)(Math.random () * 4);
@@ -96,15 +105,71 @@ public class Searcher {
         while (nDirection != miner.getRotation())   //While miner isnt facing the direction
             miner.rotate ();
         
-        vacateCurrentBlock ();  //Return block's original value before moving
+        moveMiner ();
+        nRandCount++;
+	}
 
+    private void moveMiner () {
+        vacateCurrentBlock ();  //Return block's original value before moving
         miner.forward ();
         cCurBlock = miner.scanCurrent(grid.getGrid ()); //Gets value of current block to be used in vacateCurrentBlock ()
 
         grid.updateMinerPosition (miner.getX (), miner.getY ());
-        addMove (randomMoveSet, nDirection);
+
+        if (bSmart)
+            addMove (smartMoveSet, miner.getRotation ());
+        else
+            addMove (randomMoveSet, miner.getRotation ());
         nRandCount++;
-	}
+    }
+
+    private int pickDirection (char[] scanned) {
+        int i;
+        int[] wantValues = new int[4];
+        int nCurDirInd = miner.getRotation () / 90;
+        int nNewDir;
+
+        for (i = 0; i < scanned.length; i++) {
+            if (scanned[i] == 'P')
+                wantValues[i] = -1;
+            else if (scanned[i] == 'B')
+                wantValues[i] = 1;
+            else if (scanned[i] == 'G') {
+                wantValues[i] = 2;
+                bFoundGoldDir = true;
+                return i;      //RETURN INSTA WHEN FOUND GOLD LETS GOOOOOOOOOOO
+            }
+            else if (scanned[i] == '\0')
+                wantValues[i] = 0;
+            else    //Edge of grid
+                wantValues[i] = -2;
+        }
+
+        nNewDir = getMaxValIndex (wantValues);
+
+        if (wantValues[nNewDir] == 0)   //If nothing dope is near
+            if (wantValues[nCurDirInd] == 0)    //And current direction has nothing bad
+                nNewDir = nCurDirInd;           //Keep going same direction, else it returns the new direction
+        else if (wantValues[nNewDir] == -1)     //If pit on every direction, we let rng do the work
+            do
+                nNewDir = (int)(Math.random () * 4);    //Pick a random direction
+            while (!(isValidDir (nNewDir * 90)));   //Makes sure random direction is valid
+
+        return nNewDir;
+    }
+
+    //Gets the index of the max value in an integer array
+    private int getMaxValIndex (int[] vals) {
+        int i;
+        int nCurInd = 1;
+        int nCurMaxInd = 0;
+        for (i = 1; i <= vals.length; i++, nCurInd++)
+            nCurInd %= 4;
+            if (vals[nCurInd] >= vals[nCurMaxInd])
+                nCurMaxInd = nCurInd;
+
+        return nCurMaxInd;
+    }
 
     //This adds the move done to the list
     private void addMove (ArrayList<Integer> moveSet, int nDirection) {
@@ -116,15 +181,15 @@ public class Searcher {
         if (dir == 0)           //Right
             if (miner.getX () == n - 1) return false;
             else return true;
-        else if (dir == 90)     //Up
-            if (miner.getY () == 0) return false;
-            else return true;
+        else if (dir == 90)    //Down
+        if (miner.getY () == n - 1) return false;
+        else return true;
         else if (dir == 180)    //Left
             if (miner.getX () == 0) return false;
             else return true;
-        else if (dir == 270)    //Down
-            if (miner.getY () == n - 1) return false;
-            else return true;
+        else if (dir == 270)     //Up
+        if (miner.getY () == 0) return false;
+        else return true;
         
         return false;   //return false if for sum reason direction is invalid
     }
